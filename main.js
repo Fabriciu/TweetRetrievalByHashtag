b9b5d6366001e11544ff3444722546cf818bbb7d
Fabrício Borgatto <fabricio.borgatto@gmail.com>
2019-08-18 13:09:28 -0300

    atualização

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 let axios = require('axios');
-
 let config = require('./config');
 
 let MongoClient = require('mongodb').MongoClient;
@@ -11,61 +10,33 @@ const headers = {
   "Authorization": "Bearer " + config.accessToken
 };
 
-//const hashtag = encodeURIComponent(config.hashtags[0]);
-
-//const hashtags = []
-
 for(var hashtag of config.hashtags) {
 
-	hashtag = encodeURIComponent(hashtag)
+	var url = config.url;
 
-	var url = config.url + "?q=" + hashtag + "&result_type=" + config.resultType + "&count=" + config.count;
-
-	//console.log("Recuperando os ultimos tweets com a hashtag " + hashtag)
-
-	//console.log("url: " + url)
-
-	axios.get(url, {headers})
-	  .then(response => {
-	    //console.log(response.data)
+	axios.get(url, {
+		params: {
+			q: hashtag,
+			result_type: config.resultType,
+			count: config.count
+		}, 
+		headers})
+	  .then(function(response) {
 	    var lastTweets = []
-	    //console.log("lastTweets size = " + lastTweets.length)
-	    //var count = 1;
-	    //console.log("Valores a inserir na base")
+	    console.log("Gravando os ultimos tweets com a hashtag " + response.config.params.q)
 	    for(var status of response.data.statuses) {
-	    	/*console.log("\n\nitem: " + count)
-	    	console.log(status.id)
-	    	console.log(status.created_at)
-	    	console.log(status.user.id)
-	    	console.log(status.user.name)
-	    	console.log(status.user.screen_name)
-	    	console.log(status.user.followers_count)
-	    	console.log(status.lang)
-	    	console.log(status.user.user_location)
-	    	console.log(config.hashtags[0])
-	    	count++*/
 	    	var tweet = {id: status.id,
-	    		created_at: status.created_at,
+	    		created_at: new Date(status.created_at),
 	    		user_id: status.user.id,
 	    		user_name: status.user.name,
 	    		user_screen_name: status.user.screen_name,
 	    		user_followers_count: status.user.followers_count,
 	    		lang: status.lang,
 	    		user_location: status.user.location,
-	    		hashtag: config.hashtags[0]
+	    		hashtag: response.config.params.q
 	    	}
 	    	lastTweets.push(tweet)
-	    	/*console.log(tweet.id)
-	    	console.log(tweet.created_at)
-	    	console.log(tweet.user_id)
-	    	console.log(tweet.user_name)
-	    	console.log(tweet.user_screen_name)
-	    	console.log(tweet.user_followers_count)
-	    	console.log(tweet.lang)
-	    	console.log(tweet.user_location)
-	    	console.log(tweet.hashtag)*/
 	    }
-		console.log("lastTweets size = " + lastTweets.length)
 		if(lastTweets.length > 0) {
 		    MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
 		    	if(err) throw err;
@@ -78,7 +49,7 @@ for(var hashtag of config.hashtags) {
 		    })
 		}
 	  })
-	  .catch(error => {
+	  .catch(function(error) {
 	    console.log(error)
 	  });
 
